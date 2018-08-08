ab62dac5a85b3edf400a7babad45f76dc346488c
mattfreire <freire1996@gmail.com>
2018-08-08 23:22:05 +0200

    create, update, delete views added

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
+    description = models.CharField(max_length=150, blank=True, null= True)
 
     def __str__(self):
         return self.title
--- frontend/gui/src/containers/ArticleDetailView.js
+++ frontend/gui/src/containers/ArticleDetailView.js
@@ -1,31 +1,50 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import { Card } from 'antd';
+import { Button, Card } from "antd";
+
+import CustomForm from "../components/Form";
 
 class ArticleDetail extends React.Component {
+  state = {
+    article: {},
+  };
 
-    state = {
-        article: {}
-    }
+  componentDidMount() {
+    const articleID = this.props.match.params.articleID;
+    axios.get(`http://127.0.0.1:8000/api/${articleID}`).then((res) => {
+      this.setState({
+        article: res.data,
+      });
+    });
+  }
 
-    componentDidMount() {
-        const articleID = this.props.match.params.articleID;
-        axios.get(`http://127.0.0.1:8000/api/${articleID}`)
-            .then(res => {
-                this.setState({
-                    article: res.data
-                });
-            })
-    }
+  handleDelete = (event) => {
+    const articleID = this.props.match.params.articleID;
+    axios.delete(`http://127.0.0.1:8000/api/${articleID}`);
+    this.props.history.push("/");
+    this.forceUpdate();
+  };
 
-    render() {
-        return (
-            <Card title={this.state.article.title}>
-                <p>{this.state.article.content}</p>
-            </Card>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Card title={this.state.article.title}>
+          <p>{this.state.article.content}</p>
+        </Card>
+        <CustomForm
+          requestType="put"
+          articleID={this.props.match.params.articleID}
+          btnText="Update"
+        />
+        <form onSubmit={this.handleDelete}>
+          <Button type="danger" htmlType="submit">
+            Delete
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default ArticleDetail;
